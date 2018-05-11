0da08735d287ab46ef79d09747312703fdd33844
Arturo Galan <arturo.galan@gmail.com>
2018-05-12 00:39:20 +0200

    slider

--- src/components/MasterControl.vue
+++ src/components/MasterControl.vue
@@ -1,11 +1,14 @@
 <template>
   <div>
       hola
-</div>
+      <vue-slider-nb>
+      </vue-slider-nb>  
+  </div>
+
 </template>
 
 <script>
-import KnobGrid from './KnobGrid.vue'
+import VueSliderNb from './VueSliderNb.vue'
 export default {
   name: 'MasterControl',
   props: [],
@@ -15,12 +18,9 @@ export default {
         };
     },
     methods: {
-        deleteClick() {
-            this.$emit('delete');
-        }
     },
     components: {
-        KnobGrid
+        VueSliderNb
     }
 }
 </script>
@@ -28,58 +28,4 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
 
-.node-card {
-    display: flex;
-    position: relative;
-    flex-direction: column;
-    margin: 5px;
-    padding: 15px 15px 10px 15px;
-    border: solid 1px rgb(122, 44, 44);
-}
-
-.node-card.removing {
-    background-color: red;
-}
-
-.node-card  {
-    position: relative;
-    top: 0px;
-    right: 5px;
-    height: 274px;
-    width: 180px;
-}
-.delete {
-    position: absolute;
-    right: 0;
-    top: 0;
-    cursor: pointer;
-    font-family: 'FontPbio';
-}
-.node-card .attack-buttons {
-    display: flex;
-    justify-content: space-between;
-}
-
-.node-name {
-    font-family: 'FontPbio';
-    font-size: small;
-    position: relative;
-    top: 50%;
-    z-index: 100;
-}
-  @font-face {
-    font-family: "FontPbio";
-    src: url('../assets/fonts/pbio-bold.ttf') format("truetype");
-    font-weight: 400;
-    font-style: normal;
-  }
-
-.pedal-svg {
-    height: 270px;
-    width: 180px;
-    left: 0;
-    top: 0;
-    z-index: 50;
-    border-radius: 5px;
-}
 </style>
---
+++ src/components/VueSliderNb.vue
@@ -0,0 +1,73 @@
+<template>
+<div>
+  <p>Custom range slider:</p>
+<div class="slidecontainer">
+  <input type="range" min="1" max="100" value="50" class="slider" orient="vertical" id="myRange">
+  <input type="range" min="1" max="100" value="50" class="slider" orient="vertical" id="myRange">
+  <input type="range" min="1" max="100" value="50" class="slider" orient="vertical" id="myRange">
+  <input type="range" min="1" max="100" value="50" class="slider" orient="vertical" id="myRange">
+
+</div>
+</div>
+
+</template>
+<script>
+export default {
+  template:'#vue-slider-nb'
+}
+</script>
+
+<style>
+
+.slidecontainer {
+    display: grid;
+    /* grid-template-columns: 25% 25% 25% 25%; */
+    margin-left: 15%;
+}
+
+.slider {
+    width:300px;
+   -webkit-appearance: none;
+    height: 25%;
+    transform: translateY(30px) rotate(90deg);
+    transform-origin: 0;
+    border-radius: 5px;   
+    opacity: 0.7;
+    -webkit-transition: .2s;
+    transition: opacity .2s;
+    background: lightskyblue;
+}
+
+/* .slider::-webkit-slider-runnable-track {
+    background: yellow;  
+    border: 1px solid black;  
+}*/
+
+.slider::-webkit-slider-thumb {
+    -webkit-appearance: none;
+    width: 25px;
+    height: 25px;
+    border-radius: 50%;
+    background: #4CAF50;
+    cursor: pointer;
+}
+
+
+/* .slider::-webkit-slider-thumb {
+    appearance: none;
+    width: 25px;
+    height: 25px;
+    border-radius: 50%; 
+    background: #4CAF50;
+    cursor: pointer;
+} */
+
+/* .slider::-moz-range-thumb {
+    width: 25px;
+    height: 25px;
+    border-radius: 50%;
+    background: #4CAF50;
+    cursor: pointer;
+    -webkit-appearance: slider-vertical;
+} */
+</style>
