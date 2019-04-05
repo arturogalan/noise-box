ed8cf66c2cfd59799fc7cdba358a9d4b10ead812
Arturo Galan <arturo.galan@clarity.ai>
2019-04-05 23:14:04 +0200

    changed to noisify

--- src/helpers/audioUtils.js
+++ src/helpers/audioUtils.js
@@ -1,22 +1,22 @@
 import {capitalize} from 'lodash'
 import irf from 'file-loader!../audio/hall-reverb.ogg';
-import * as AudioEffects from 'audio-effects';
+import * as Noisefy from 'noisefy';
 import {PEDAL_TYPE} from '../store/constants';
 
 const audioUtils = {
  createAudioContext(){
-    if (AudioEffects.HasAudioContext) {
+    if (Noisefy.hasAudioContext) {
       // return new AudioContext({ latencyHint: 'playback' });
       return new AudioContext();
     }
   },
   createAudioNode(audioContext, type){
-    let audioNode = new AudioEffects[capitalize(type)](audioContext);
+    let audioNode = new Noisefy[capitalize(type)](audioContext);
     return audioNode;
   },
   configAudioNode(pedal){
     if(pedal.type === PEDAL_TYPE.REVERB) {
-      AudioEffects.Reverb.getInputResponseFile(irf).then(buffer => {
+      Noisefy.Reverb.getInputResponseFile(irf).then(buffer => {
         console.log('entra')
         if(!pedal.effect.buffer){
           pedal.effect.buffer = buffer;
@@ -27,39 +27,23 @@ const audioUtils = {
     }
   },
   createInput(audioContext){
-    const input = new AudioEffects.Input(audioContext);
+    const input = new Noisefy.Input(audioContext);
     debugger;
     let audioSource = '';
-    const initConfig = {
-      audio: {
-          optional: [
-            {
-              "echoCancellation" : false,
-            },
-            {
-              "mozNoiseSuppression": false,
-            },
-            {
-              "mozAutoGainControl": false,
-            },
 
-          ]
-      }
-  };
-    input.getUserMedia(initConfig).then(function (stream) {
-      window.stream = stream; // make stream available to console
-      // Refresh button list in case labels have become available
-  })
-  .then(()=> {
+    input.getUserMedia();
+  //   .then(function (stream) {
+  //     window.stream = stream; // make stream available to console
+  //     // Refresh button list in case labels have become available
+  // })
 
-  });
     // input.level = 0.5;
     // input.mute = false;
     console.log('latency',audioContext.baseLatency);
     return convertToMono(input, audioContext);
   },
   createOutput(audioContext){
-    return new AudioEffects.Output(audioContext);
+    return new Noisefy.Output(audioContext);
   }
 };
 
