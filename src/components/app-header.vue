d97fe87eedab78b1b52cff57dae9a0a9e431d08b
Arturo Galan <arturo.galan@clarity.ai>
2019-05-19 02:18:30 +0200

    header metal grid

--- src/components/app-header.vue
+++ src/components/app-header.vue
@@ -1,5 +1,9 @@
 <script>
+import metalGrid from './common/metal-grid.vue';
 export default {
+  components: {
+    metalGrid,
+  },
   data() {
     return {
     };
@@ -13,8 +17,15 @@ export default {
 </script>
 
 <template>
-  <div id="grad1">
-    <div v-tooltip="{text: 'F*king awesome noise box!!!'}">
+  <div>
+    <div class="grid-wrapper">
+      <metal-grid
+        v-for="i in 225"
+        :key="i"/>
+    </div>
+    <div
+      v-tooltip="{text: 'F*king awesome noise box!!!'}"
+      class="index-2">
       <img
         src="/static/header-icon.png"
         class="logo-icon">
@@ -25,7 +36,12 @@ export default {
 </template>
 
 <style>
-#grad1 {
+.grid-wrapper {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+}
+.grad1 {
   height: 140px;
   background: #4c11ac; /* For browsers that do not support gradients */
   background: linear-gradient(
@@ -35,6 +51,12 @@ export default {
   ); /* Standard syntax (must be last) */
   margin: 0px;
 }
+/* .header-grid {
+  height: 140px;
+  background-size: 15px 15px;
+  background-color: black;
+  background-image: linear-gradient(to right,rgb(42,39,38) 3px, transparent 1px), linear-gradient(to bottom,rgb(42,39,38) 3px, transparent 1px);
+} */
 h1 {
   text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
   font-size: 7em;
---
+++ src/components/common/metal-grid.vue
@@ -0,0 +1,128 @@
+<script>
+export default {
+  name: 'MetalGrid',
+
+};
+</script>
+<template>
+  <div class="metal-grid-wrapper n-index-5">
+    <div class="horizontal-metal-bar horizontal-metal-bar--big top n-index-3">
+      <div class="horizontal-white-reflect horizontal-white-reflect--middle-left"/>
+      <div class="horizontal-white-reflect horizontal-white-reflect--middle-right"/>
+    </div>
+    <div class="horizontal-metal-bar horizontal-metal-bar--small center left-small n-index-1">
+      <div class="horizontal-white-reflect"/>
+      <div class="horizontal-white-reflect horizontal-white-reflect--right"/>
+    </div>
+    <div class="horizontal-metal-bar horizontal-metal-bar--big bottom n-index-3">
+      <div class="horizontal-white-reflect horizontal-white-reflect--middle-left"/>
+      <div class="horizontal-white-reflect horizontal-white-reflect--middle-right"/>
+    </div>
+    <div class="vertical-metal-bar left n-index-4">
+      <div class="vertical-white-reflect vertical-white-reflect--top"/>
+    </div>
+    <div class="vertical-metal-bar middle n-index-2">
+      <div class="vertical-white-reflect vertical-white-reflect--middle"/>
+    </div>
+    <div class="vertical-metal-bar right n-index-4">
+      <div class="vertical-white-reflect vertical-white-reflect--top"/>
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped>
+  $metal-color: rgb(48,43,43);
+  $metal-border-color: rgb(17,10,8);
+  $box-shadow: #4f4848;
+  $box-shadow-2: #292828;
+
+  .metal-grid-wrapper {
+    position: relative;
+    width: 2.5rem;
+    height: 2.5rem;
+    background-color: black;
+    display: flex;
+    overflow: hidden;
+    float: left;
+  }
+  .horizontal-metal-bar {
+    position: absolute;
+    background-color: $metal-color;
+    border-top: 3px solid $metal-border-color;
+    border-bottom: 3px solid $metal-border-color;
+    &--big {
+      width: 100%;
+      height: 10%;
+    }
+    &--small {
+      width: 83%;
+      height: 10%;
+      border-left: 3px solid $metal-border-color;
+      border-right: 3px solid $metal-border-color;
+    }
+      box-shadow: inset -7px -2px 4px 0px #110a08;
+  }
+  .vertical-metal-bar {
+    position: absolute;
+    background-color: $metal-color;
+    border-left: 3px solid $metal-border-color;
+    width: 10%;
+    height: 100%;
+    border-right: 3px solid $metal-border-color;
+  }
+  .center {
+    align-self: center;
+  }
+  .top {
+    top: 0;
+    transform: translateY(-50%)
+  }
+  .bottom {
+    bottom: 0;
+    transform: translateY(50%)
+  }
+  .left {
+    left:0;
+    transform: translateX(-50%)
+  }
+  .left-small{
+    left:5%;
+  }
+  .right {
+    right:0;
+    transform: translateX(50%)
+  }
+  .middle {
+    left: 44%;
+  }
+  .horizontal-white-reflect{
+    position: absolute;
+    background-color: white;
+    border-radius: 20%;
+    height: 80%;
+    width: 20%;
+    box-shadow: inset 1px -1px 8px 5px $box-shadow-2;
+    &--right {
+      right:0;
+    }
+    &--middle-left {
+      left: 25%;
+    }
+    &--middle-right {
+      left: 55%;
+    }
+  }
+  .vertical-white-reflect{
+    position: absolute;
+    background-color: white;
+    border-radius: 20%;
+    height: 20%;
+    width: 100%;
+    box-shadow: inset 0px 2px 6px 4px $box-shadow-2;
+    &--middle {
+      top:55%;
+    }
+    &--top{
+      top: 0;
+    }
+  }
+</style>
