34509822901bb9f0ec6f91c391370e0341c19b90
Arturo Galan <arturo.galan@gmail.com>
2020-09-24 21:54:45 +0200

    MainMenuAndStylophone

---
+++ src/views/MainMenu.vue
@@ -0,0 +1,38 @@
+<script>
+
+export default {
+  name: 'MainMenu',
+  components: {
+  },
+  mixins: [],
+  data () {
+    return {
+    }
+  },
+  computed: {
+
+  },
+  created () {
+
+  },
+  mounted () {
+  },
+  methods: {
+
+  },
+
+}
+</script>
+<template>
+  <div>
+    This is the main menu
+    <router-link :to="{name: 'guitarAmp'}">
+      GUITAR AMP
+    </router-link>
+    <router-link :to="{name: 'stylophone'}">
+      STYLOPHONE
+    </router-link>
+  </div>
+</template>
+<style lang="scss">
+</style>
---
+++ src/views/Stylophone.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    this is the stylophone
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Stylophone',
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
